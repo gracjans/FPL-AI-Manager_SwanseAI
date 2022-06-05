1baa1b0bbe759db5548ef629f77f76250a86eb7c
gracjans <gracjanss98@gmail.com>
2022-06-05 03:13:53 +0200

    update data_loader to support relative path

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,9 +3,9 @@ import pandas
 
 
 def load_data(season):
+    root_dir = os.path.dirname(os.path.dirname(__file__))
+    data_path = root_dir + f"\\data\\Fantasy-Premier-League\\{season}\\gws\\merged_gw.csv"
     try:
-        data_path = os.getcwd() + "\\data\\Fantasy-Premier-League"
-        merged_gw_2021_path = data_path + f"\\{season}\\gws\\merged_gw.csv"
-        return pandas.read_csv(merged_gw_2021_path, encoding='latin-1')
+        return pandas.read_csv(data_path, encoding='latin-1')
     except FileNotFoundError:
         raise FileNotFoundError("ERROR: Please enter valid season! Available seasons range from '2016-17' to '2021-22'")
