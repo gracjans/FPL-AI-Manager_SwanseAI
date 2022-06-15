6e7beca773cfb688c3f8bb82f23d3e942c180f03
gracjans <gracjanss98@gmail.com>
2022-06-15 20:41:10 +0200

    update baseline data processing pipeline

--- src/features/data_engineering.py
+++ src/features/data_engineering.py
@@ -4,6 +4,20 @@ from sklearn.model_selection import train_test_split
 from src.data.data_loader import load_raw_data
 
 
+def reverse_processing(x_data, x_data_scaler, extracted_target=None):
+    """
+    Reverses the preprocessing of the data and optionally concatenates it with the extracted target data
+    """
+
+    # reverse scaling of x data
+    x = pd.DataFrame(x_data_scaler.inverse_transform(x_data), index=x_data.index, columns=x_data.columns)
+
+    if extracted_target is not None:
+        return pd.concat([extracted_target, x], axis=1)
+    else:
+        return x
+
+
 def process_baseline_data(season):
     """
     Preprocesses the data for the baseline model.
@@ -15,15 +29,8 @@ def process_baseline_data(season):
     # add 'total_points_next_gameweek' column where total_points_next_gameweek = total_points from next 'GW' for each 'element'
     data['total_points_next_gameweek'] = data.groupby('element')['total_points'].shift(-1)
 
-    # create data_2021_processed dataframe without 'name', 'team', 'fixture', 'kickoff_time', 'opponent_team', 'round', 'team_h_score', 'team_a_score'
-    data_processed = data.drop(
-        ['name', 'team', 'fixture', 'kickoff_time', 'opponent_team', 'round', 'team_h_score', 'team_a_score'], axis=1)
-
-    # extract 'GW', 'element' and 'total_points_next_gameweek' from data_2021_processed
-    data_extract_target = data_processed[['GW', 'element', 'total_points_next_gameweek']]
-
-    # drop 'GW' and 'element' from data_2021_processed
-    data_processed = data_processed.drop(['GW', 'element'], axis=1)
+    # create data_2021_processed dataframe without 'team', 'fixture', 'kickoff_time', 'opponent_team', 'round', 'team_h_score', 'team_a_score'
+    data_processed = data.drop(['team', 'fixture', 'kickoff_time', 'opponent_team', 'round', 'team_h_score', 'team_a_score'], axis=1)
 
     # one-hot encode 'position' column
     data_processed = pd.get_dummies(data_processed, columns=['position'])
@@ -32,16 +39,30 @@ def process_baseline_data(season):
     data_processed['was_home'] = data_processed['was_home'].map({True: 1, False: 0})
 
     # drop rows with NaN values
-    data_2021_processed = data_processed.dropna()
+    data_processed = data_processed.dropna()
 
-    X = data_2021_processed.drop(['total_points_next_gameweek'], axis=1)
-    y = data_2021_processed['total_points_next_gameweek']
+    # extract 'name', 'GW', 'element' and 'total_points_next_gameweek' from data_processed
+    data_extract_target = data_processed[['name', 'GW', 'element', 'total_points_next_gameweek']]
 
-    # scale X data
-    X_scaler = MinMaxScaler(feature_range=(0, 1))
-    X_2021_scaled = pd.DataFrame(X_scaler.fit_transform(X), columns=X.columns)
+    # prepare x, y data
+    x = data_processed.drop(['name', 'GW', 'element', 'total_points_next_gameweek'], axis=1)
+    y = data_processed['total_points_next_gameweek']
+
+    # scale x data
+    x_scaler = MinMaxScaler(feature_range=(0, 1))
+    x_data_scaled = pd.DataFrame(x_scaler.fit_transform(x), index=x.index, columns=x.columns)
+
+    # concatenate data_extract_target and x_data_scaled
+    x_data_scaled = pd.concat([data_extract_target, x_data_scaled], axis=1)
 
     # split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X_2021_scaled, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x_data_scaled, y, test_size=0.2, random_state=42)
 
-    return X_train, X_test, y_train, y_test
+    # extract target and drop it from x data
+    x_train_data_extract_target = x_train[['name', 'GW', 'element', 'total_points_next_gameweek']]
+    x_train.drop(['name', 'GW', 'element', 'total_points_next_gameweek'], axis=1, inplace=True)
+
+    x_test_data_extract_target = x_test[['name', 'GW', 'element', 'total_points_next_gameweek']]
+    x_test.drop(['name', 'GW', 'element', 'total_points_next_gameweek'], axis=1, inplace=True)
+
+    return (x_train, y_train), (x_test, y_test), (x_train_data_extract_target, x_test_data_extract_target), x_scaler
