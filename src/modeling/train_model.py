3689273bef5469d4a6e8938e8f05c709cec095ab
gracjans <gracjanss98@gmail.com>
2022-07-06 19:13:38 +0200

    add type hinting for train_model script

--- src/modeling/train_model.py
+++ src/modeling/train_model.py
@@ -1,7 +1,12 @@
-def train_mlp_model(model, x_train, y_train, batch_size=16, epochs=30, validation_split=0.2, optimizer='adam',loss='mse'):
+import numpy as np
+
+
+def train_mlp_model(model: object, x_train: np.array, y_train: np.array, batch_size: int = 16,
+                    epochs: int = 30, validation_split: float = 0.2, optimizer: str = 'adam', loss: str = 'mse'):
     """
     Train multilayer perceptron model on given data and return trained model
     """
+
     model.compile(optimizer=optimizer, loss=loss)
     model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
     return model
