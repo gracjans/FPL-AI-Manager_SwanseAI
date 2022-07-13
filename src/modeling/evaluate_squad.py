a63e21c14bc944c8ded5ded756d06042691c3fe1
gracjans <gracjanss98@gmail.com>
2022-07-13 21:30:49 +0200

    refactor code of getting best 11

--- src/modeling/evaluate_squad.py
+++ src/modeling/evaluate_squad.py
@@ -16,14 +16,15 @@ def squad_selection_without_constraints(predictions_merged: pd.DataFrame, season
     # get first row from predictions_merged and double 'total_points_next_gameweek' value, because this player would be chosen as a capitan
     predictions_merged.iloc[0, predictions_merged.columns.get_loc('total_points_next_gameweek')] *= 2
 
-    # get goalkeepers from predictions_merged_27 (with 1 in 'position_GK' column)
-    df_gk = predictions_merged[predictions_merged.position_GK == 1]
-    df_def = predictions_merged[predictions_merged.position_DEF == 1]
-    df_mid = predictions_merged[predictions_merged.position_MID == 1]
-    df_fwd = predictions_merged[predictions_merged.position_FWD == 1]
-
-    # get one top row from df_gk, three top from df_def, five top from df_mid, two top from df_fwd and concatenate them into one dataframe
-    df_top_11 = pd.concat([df_gk.head(1), df_def.head(3), df_mid.head(5), df_fwd.head(2)])
+    # separate players by position and get dataframe with best squad in given formation
+    df_top_11 = pd.DataFrame()
+    positions = ['position_GK', 'position_DEF', 'position_MID', 'position_FWD']
+    formation = [1, 3, 5, 2]
+    for i, position in enumerate(positions):
+        # get players with the given position
+        players = predictions_merged[predictions_merged[position] == 1]
+        # add players to df_top_11 dataframe
+        df_top_11 = df_top_11.append(players.head(formation[i]), ignore_index=True)
 
     # get 'name', 'total_points_next_gameweek', 'transfers_balance', 'value' columns from df_top_11
     df_squad = df_top_11[['name', 'total_points_next_gameweek', 'transfers_balance', 'value']]
