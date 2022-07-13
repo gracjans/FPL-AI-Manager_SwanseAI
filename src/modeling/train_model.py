99880ce04edee339afad444ed8f6061af2d6a16c
gracjans <gracjanss98@gmail.com>
2022-07-14 01:07:27 +0200

    add verbose parameter to train mlp model function

--- src/modeling/train_model.py
+++ src/modeling/train_model.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def train_mlp_model(model: object, x_train: np.array, y_train: np.array, batch_size: int = 16,
-                    epochs: int = 30, validation_split: float = 0.2, optimizer: str = 'adam', loss: str = 'mse'):
+def train_mlp_model(model: object, x_train: np.array, y_train: np.array, batch_size: int = 16, epochs: int = 30,
+                    validation_split: float = 0.2, optimizer: str = 'adam', loss: str = 'mse', verbose: int = 1):
     """
     Train multilayer perceptron model on given data and return trained model
     """
 
     model.compile(optimizer=optimizer, loss=loss)
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=validation_split, verbose=verbose)
     return model
