dab1f565f34b537f7c787fb8bef4fad16cd76831
gracjans <gracjanss98@gmail.com>
2022-07-24 22:30:44 +0200

    add get_league_table function to dat_loader

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,5 +1,10 @@
 import os
+import aiohttp
 import pandas as pd
+from understat import Understat
+
+import nest_asyncio
+nest_asyncio.apply()
 
 
 def load_merged_gw(season: str):
@@ -25,3 +30,10 @@ def load_average_pts():
     root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     data_path = root_dir + f"\\data\\external\\average_pts.csv"
     return pd.read_csv(data_path, encoding='latin-1')
+
+
+async def get_league_table(season, start_date=None, end_date=None):
+    async with aiohttp.ClientSession() as session:
+        understat = Understat(session)
+        table = await understat.get_league_table("EPL", season, start_date=start_date, end_date=end_date)
+        return pd.DataFrame(table[1:], columns=table[0])
