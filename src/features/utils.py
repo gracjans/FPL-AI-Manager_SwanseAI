b5ad400bafe58b548cf66f8587011ca4ef24f8af
gracjans <gracjanss98@gmail.com>
2022-07-25 14:56:59 +0200

    add typehinting to utils

--- src/features/utils.py
+++ src/features/utils.py
@@ -1,11 +1,13 @@
 import datetime
 
+import pandas as pd
 
-def idx_to_team_name(master_team_list, team, season):
+
+def idx_to_team_name(master_team_list: pd.DataFrame, team: int, season: str):
     # get team name from master_team_list with appropriate season and team
     return master_team_list[(master_team_list['season'] == season) & (master_team_list['team'] == team)]['team_name'].values[0]
 
 
-def str_date_months_back(date, months_back):
+def str_date_months_back(date: str, months_back: int):
     # get date months back from date
     return (datetime.datetime.strptime(date, '%Y-%m-%d') - datetime.timedelta(days=months_back * 30)).strftime('%Y-%m-%d')
