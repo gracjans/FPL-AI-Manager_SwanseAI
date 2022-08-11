2a5148ccb256d6078d8232957ac0b528334f5e1a
gracjans <gracjanss98@gmail.com>
2022-08-11 19:28:56 +0200

    update downloading fpl file script

--- src/squad_selection/predict_and_select.py
+++ src/squad_selection/predict_and_select.py
@@ -11,13 +11,24 @@ from src.squad_selection.select_team import select_team, print_selected_team
 
 
 def download_newest_fpl_data(season: str):
+    print('Downloading newest FPL data')
+
     destination = Path(os.path.dirname(os.getcwd()) + f'\\data\\raw\\Fantasy-Premier-League\\{season}')
     destination.mkdir(exist_ok=True, parents=True)
-    fs = fsspec.filesystem("github", org="vaastav", repo="Fantasy-Premier-League")
-    fs.get(fs.ls(f"data/{season}/"), destination.as_posix(), recursive=True)
+
+    fs = fsspec.filesystem("github", org="vaastav", repo="Fantasy-Premier-League", username='gracjans', token='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')
+    listed_files = fs.ls(f"data/{season}/")
+
+    # do not download folders with many data that is not used
+    listed_files.remove(f'data/{season}/players')
+    listed_files.remove(f'data/{season}/understat')
+
+    fs.get(listed_files, destination.as_posix(), recursive=True)
+
+    print('Download complete')
 
 
-def predict_and_select(season: str, gameweek: int, model_path_from_root: str, scaler_path_from_root: str):
+def predict_and_select_team(season: str, gameweek: int, model_path_from_root: str, scaler_path_from_root: str):
     download_newest_fpl_data(season)
 
     rolling_columns = ['assists', 'bonus', 'bps', 'clean_sheets',
@@ -53,3 +64,12 @@ def predict_and_select(season: str, gameweek: int, model_path_from_root: str, sc
                                                               prediction_df_sum.team.values, total_budget=100, sub_factor=0.15)
 
     print_selected_team(prediction_df_sum, decisions, captain_decisions, sub_decisions)
+
+
+def predict_and_select_transfer(season: str, gameweek: int, model_path_from_root: str, scaler_path_from_root: str):
+    download_newest_fpl_data(season)
+
+    rolling_columns = ['assists', 'bonus', 'bps', 'clean_sheets',
+                       'creativity', 'goals_conceded', 'goals_scored',
+                       'ict_index', 'influence', 'minutes',
+                       'saves', 'selected', 'player_team_score', 'opponent_team_score', 'threat']
