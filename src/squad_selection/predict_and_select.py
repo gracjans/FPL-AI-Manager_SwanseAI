ab81632feeacb778a6c687d8c6329f88ca9ee3d0
gracjans <gracjanss98@gmail.com>
2022-08-16 23:55:21 +0200

    add positibiliy to predict_and_select for separated field

--- src/squad_selection/predict_and_select.py
+++ src/squad_selection/predict_and_select.py
@@ -14,12 +14,15 @@ from src.squad_selection.select_transfer import TransferOptimiser, print_selecte
 
 
 def download_newest_fpl_data(season: str):
+    root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    secret = open(root_dir + "/secret.txt", "r")
+
     print('Downloading newest FPL data')
 
     destination = Path(os.path.dirname(os.getcwd()) + f'\\data\\raw\\Fantasy-Premier-League\\{season}')
     destination.mkdir(exist_ok=True, parents=True)
 
-    fs = fsspec.filesystem("github", org="vaastav", repo="Fantasy-Premier-League", username='gracjans', token='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')
+    fs = fsspec.filesystem("github", org="vaastav", repo="Fantasy-Premier-League", username='gracjans', token=secret.read())
     listed_files = fs.ls(f"data/{season}/")
 
     # do not download folders with many data that is not used
@@ -110,8 +113,9 @@ def predict_and_select_team(season: str, gameweek: int, model_path_from_root: st
     print_selected_team(prediction_df_sum, decisions, captain_decisions, sub_decisions)
 
 
-def predict_and_select_transfer(season: str, gameweek: int, model_path_from_root: str, scaler_path_from_root: str,
-                                download_newest_data: bool = True, get_actual_chance_playing: bool = True):
+def predict_and_select_transfer(season: str, gameweek: int, model_path_from_root: list, scaler_path_from_root: list,
+                                download_newest_data: bool = True, get_actual_chance_playing: bool = True, position_separated: bool = False):
+    """Model path from root = ['gk model path', 'field model path'] if position separated is True"""
     # TODO: merge this function with predict_and_select_team, it's almos the same!
     if download_newest_data:
         download_newest_fpl_data(season)
@@ -123,16 +127,46 @@ def predict_and_select_transfer(season: str, gameweek: int, model_path_from_root
                        'total_points', 'transfers_in', 'transfers_out',
                        'value', 'yellow_cards']
 
+    rolling_columns_gk = ['bonus', 'bps', 'clean_sheets', 'goals_conceded', 'influence', 'minutes',
+                          'penalties_saved', 'saves', 'selected', 'player_team_score', 'opponent_team_score',
+                          'total_points', 'transfers_in', 'transfers_out','value']
+
+    rolling_columns_field = ['assists', 'bonus', 'bps', 'clean_sheets', 'creativity', 'goals_conceded', 'goals_scored',
+                             'ict_index', 'influence', 'minutes', 'selected', 'player_team_score', 'opponent_team_score',
+                             'threat', 'total_points', 'transfers_in', 'transfers_out', 'value', 'yellow_cards']
+
     times = ['all', 6, 3]
-
-    x_prediction, x_target = preprocess_prediction_data(season, gameweek, rolling_columns=rolling_columns, rolling_times=times, opponent_team_stats=True)
-
     root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    model = tf.keras.models.load_model(root_dir + model_path_from_root)
-    x_scaler = joblib.load(root_dir + scaler_path_from_root)
 
-    y_pred = pd.Series(model.predict(x_scaler.transform(x_prediction)).reshape(-1, ), index=x_prediction.index, name='predicted_total_points_next_gameweek')
-    prediction_df = pd.concat([y_pred, x_target], axis=1)
+    if position_separated:
+        x_prediction_field, x_target_field = preprocess_prediction_data(season, gameweek, rolling_columns=rolling_columns_field,
+                                                                        rolling_times=times, opponent_team_stats=True, position='field')
+        model_field = tf.keras.models.load_model(root_dir + model_path_from_root[1])
+        x_scaler_field = joblib.load(root_dir + scaler_path_from_root[1])
+
+        y_pred_field = pd.Series(model_field.predict(x_scaler_field.transform(x_prediction_field)).reshape(-1, ), index=x_prediction_field.index, name='predicted_total_points_next_gameweek')
+        prediction_df_field = pd.concat([y_pred_field, x_target_field], axis=1)
+
+        x_prediction_gk, x_target_gk = preprocess_prediction_data(season, gameweek, rolling_columns=rolling_columns_gk,
+                                                                  rolling_times=times, opponent_team_stats=True, position='gk')
+        model_gk = tf.keras.models.load_model(root_dir + model_path_from_root[0])
+        x_scaler_gk = joblib.load(root_dir + scaler_path_from_root[0])
+
+        y_pred_gk = pd.Series(model_gk.predict(x_scaler_gk.transform(x_prediction_gk)).reshape(-1, ), index=x_prediction_gk.index, name='predicted_total_points_next_gameweek')
+        prediction_df_gk = pd.concat([y_pred_gk, x_target_gk], axis=1)
+
+        prediction_df = pd.concat([prediction_df_gk, prediction_df_field], axis=1)
+        return prediction_df_gk, prediction_df_field
+
+    else:
+        x_prediction, x_target = preprocess_prediction_data(season, gameweek, rolling_columns=rolling_columns,
+                                                            rolling_times=times, opponent_team_stats=True, position='all')
+
+        model = tf.keras.models.load_model(root_dir + model_path_from_root[0])
+        x_scaler = joblib.load(root_dir + scaler_path_from_root[0])
+
+        y_pred = pd.Series(model.predict(x_scaler.transform(x_prediction)).reshape(-1, ), index=x_prediction.index, name='predicted_total_points_next_gameweek')
+        prediction_df = pd.concat([y_pred, x_target], axis=1)
 
     prediction_df_sum = prediction_df.groupby(['name', 'element', 'position', 'value', 'team', 'GW', 'season']).sum().reset_index()
     list_of_opponents = prediction_df.groupby(['name', 'element', 'team', 'GW', 'season'])['opponent_next_gameweek'].apply(list).reset_index()['opponent_next_gameweek']
